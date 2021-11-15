01b7548ea243c820e58a7fac0a72fe102be635e4
Jacob Welander Jensen <64834767+Welander1994@users.noreply.github.com>
2021-11-15 17:18:16 +0100

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,6 +1,46 @@
 
-<footer class="bg-gray-300">
-    <p>this is the footer</p>
+<footer class="w-full h-auto py-6 bg-primary flex flex-col justify-center text-white">
+    <div class="m-auto w-4/5 flex justify-between border-b border-gray-500">  
+        <div class="w-1/3">
+            <h1 class="pb-2 text-m uppercase font-medium w-6"><?php bloginfo( 'name' ); ?></h1>
+            <p class="py-2">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.</p>
+        </div>
+
+        <div class="flex justify-between w-3/6 text-sm">
+            <ul>
+                <li class="pb-2">Service</li>
+                <li class="pb-2">test 2</li>
+                <li class="pb-2">test 3</li>
+            </ul>
+
+            <ul>
+                <li class="pb-2">Om Virksomheden</li>
+                <li class="pb-2">test 2</li>
+                <li class="pb-2">test 3</li>
+            </ul>
+
+            <ul>
+                <li class="pb-2">Vidste du?</li>
+                <li class="pb-2">test 2</li>
+                <li class="pb-2">test 3</li>
+            </ul>
+
+            <ul>
+                <li class="pb-2"><b>Kontakt</b></li>
+                <li class="pb-2">Tlf: +45 31 51 27 60</li>
+                <li class="pb-2">info@theilmann-it.dk</li>
+                <li class="pb-2">Teglgård 1 6330 Vilsbæk</li>
+                <li class="pb-2">CVR: 42049131</li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="m-auto pt-6">
+        <p>© All rights reserved Theilmann IT/Automation</p>
+    </div>
+    
+
+
 </footer>
 
 
