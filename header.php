2484cfc2e56f9566355194a51fd277b07eaa590b
Jacob Welander Jensen <64834767+Welander1994@users.noreply.github.com>
2021-11-15 16:22:30 +0100

    header styling almost done

--- header.php
+++ header.php
@@ -8,16 +8,41 @@
 	<?php wp_head(); ?>
 </head>
 	<body <?php body_class(); ?>>
-    <header class=" flex bg-gray-300 bg-opacity-25">
-    <h1 class="text-xl uppercase"><?php bloginfo( 'name' ); ?></h1>
-        <?php $defaults = array( 
-            'container_class' => 'w-full  flex flex-wrap justify-center  content-center', 
-            'menu_class'      => 'flex flex-row justify-around', 
-            'items_wrap'      => '<ul class="flex justify-between">%3$s</ul>',
-            'add_a_class'     => 'px-5 hover:bg-green-500 uppercase',
-        ); ?>
+    <header class="bg-primary flex justify-center text-white">
 
-<?php wp_nav_menu( $defaults ); ?>
+    <div class="flex flex-col w-4/5 h-20">
+
+  
+
+        <div class="flex pl-1 flex-row justify-end w-full bg-secondary ">
+            <ul class="flex flex-row">
+                <li class="ml-5 text-white text-opacity-50">Telf: +45 31 51 27 60</li>
+                <li class="ml-5 text-white text-opacity-50">info@theilmann-IT.dk</li>
+            </ul>
+        </div>
+
+        <div class="flex flex-row justify-between w-full">
+            <div class="flex w-44 content-end">
+                    <h1 class="text-m uppercase font-medium "><?php bloginfo( 'name' ); ?></h1>
+            </div>
+            
+            <div class="self-center">
+                        <?php $defaults = array( 
+                            'container_class' => 'w-full flex flex-wrap', 
+                            'menu_class'      => 'flex flex-row justify-around ', 
+                            'items_wrap'      => '<ul class="flex">%3$s</ul>',
+                            'add_a_class'     => 'ml-5 border-b border-transparent hover:border-gray-500 transition duration-500 ease-in-out  uppercase',
+                        ); ?>
+                        <?php wp_nav_menu( $defaults ); ?>
+            </div>
+
+        </div>
+ 
+    </div>
+     
+
+
+
 
 
         
