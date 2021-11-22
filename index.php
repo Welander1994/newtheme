63e7c1bb1bc9e59dd74c9206a46d34292df4c233
Jacob Welander Jensen <64834767+Welander1994@users.noreply.github.com>
2021-11-22 11:23:35 +0100

    added styling and contex for kontakt and om virksomheden

--- index.php
+++ index.php
@@ -87,7 +87,7 @@
 				?>
 			</ul>
 					<!-- this is the section where we descript the owner -->
-					<div class="w-full h-full py-20 flex flex-col sm:flex-row justify-evenly">
+					<div class="w-full h-full py-20 flex flex-col sm:flex-row justify-evenly md:justify-center">
 						<div class="w-full sm:w-1/3 flex flex-wrap content-center order-last sm:order-none">
 							<div class="text-center pt-5 sm:pt-0 sm:text-left">
 								<a class="text-2xl font-bold capitalize mt-5" href="<?php the_permalink() ?>"><?php the_title(); ?></a>
@@ -103,10 +103,10 @@
 							</div>
 							<div class="flex flex-row m-auto py-5">
 								<a class="bg-primary p-1 px-5 m-2 rounded-m text-white text-xl font-medium uppercase hover:bg-white hover:text-black transition duration-500 ease-in-out" href="#">services</a>
-								<a class="bg-transperent p-1 px-5 m-2 border rounded-m border-primary text-xl text-primary font-medium uppercase hbg-primary hover:text-white hover:border-0 transition duration-500 ease-in-out" href="#">kontakt</a>
+								<a class="bg-transperent p-1 px-5 m-2 border rounded-m border-primary text-xl text-primary font-medium uppercase bg-primary hover:text-white hover:border-0 transition duration-500 ease-in-out" href="#">kontakt</a>
 							</div>
 						</div>
-						<div class="w-full">
+						<div class="w-fullmd:w-1/2">
 							<a href="<?php the_permalink() ?>">
 								<?php if ( has_post_thumbnail() ) :
 									$featured_image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'large' ); ?>
@@ -183,11 +183,19 @@
 		?> 
 	
 <section class="h-3/6 flex flex-col justify-center text-white text-center w-sceen">
-	<a class="text-4xl sm:text-6xl md:text-8xl p-2 uppercase font-black m-14" href="<?php the_permalink() ?>"><?php the_title(); ?></a>
+	<a class="text-4xl sm:text-4xl md:text-6xl p-2 uppercase font-black m-14" href="<?php the_permalink() ?>"><?php the_title(); ?></a>
 </section>
 		<div class="w-full bg-gray-100">
 			<div class="w-4/5 m-auto ">
-				<div class="flex justify-center">
+
+
+			<?php
+			
+	// service content 
+	global $post;
+	if( $post->ID == 13) { ?>
+		<div class="flex flex-col">
+		<div class="flex justify-center">
 					<div>
 						<?php 
 							$myContent = get_the_content();
@@ -212,13 +220,6 @@
 						</a>
 					</div>
 				</div>
-
-			<?php
-			
-	// service content 
-	global $post;
-	if( $post->ID == 13) { ?>
-		<div class="flex flex-col">
 				<div>
 					<ul class="flex flex-col sm:flex-row sm:flex-wrap md:flex-row md:flex-nowrap justify-between text-black w-full">	
 						<?php 
@@ -305,6 +306,109 @@
 	</div>
 
 	<?php } ?>
+
+	<?php
+			
+	// om virksomheden content 
+	global $post;
+	if( $post->ID == 135) { ?>
+		<div class="flex flex-col">
+			<div>
+				<div class="w-full h-full py-20 flex flex-col md:flex-row justify-evenly ">
+					<div class="w-full sm:w-1/3 flex flex-wrap order-last md:order-none">
+						<div class="text-center pt-5 sm:pt-0 sm:text-left">
+							<a class="text-2xl font-bold capitalize mt-5" href="<?php the_permalink() ?>">Om theilmann IT/Automation</a>
+							<?php 
+								$myContent = get_the_content();
+								$tags = array("<p>", "</p>");
+								$myContent = str_replace($tags, "", $myContent);
+							?>
+											
+							<p class="text-m py-5">
+								<?php   echo $myContent; ?>
+							</p>
+						</div>
+					</div>
+					<div class="w-fullmd:w-1/2">
+						<a href="<?php the_permalink() ?>">
+							<?php if ( has_post_thumbnail() ) :
+								$featured_image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'large' ); ?>
+								<img 
+									src="<?php echo $featured_image[0]; ?>" 
+									alt='' 
+									class=" w-3/4 m-auto md:w-3/4"
+								/>
+							<?php endif; ?>
+						</a>
+					</div>
+				</div>
+				<div class="w-full h-full flex flex-col ">
+					<h2 class="text-2xl font-bold capitalize my-5">Vores mål</h2>
+					<div class="flex flex-col sm:flex-row justify-between">
+						<div class="flex flex-col sm:w-3/5 border-2 bg-white p-5 sm:mr-1">
+							<h2 class="py-5 font-bold">Mission</h2>
+							<p>Stet clita kasd gubergren, no sea duo takimata sanctus est Lorem ipsum dolor sit amet t accusam et justo duo dolores et ea rebum. Lorem ipsum dolor sit amet t accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea duo.</p>
+						</div>
+						<div class="flex flex-col sm:w-3/5 border-2 bg-white p-5 sm:ml-1">
+							<h2 class="py-5 font-bold">Vision</h2>
+							<p>Stet clita kasd gubergren, no sea duo takimata sanctus est Lorem ipsum dolor sit amet t accusam et justo duo dolores et ea rebum. Lorem ipsum dolor sit amet t accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea duo.</p>
+						</div>
+					</div>
+				</div>
+				<div class="flex flex-col justify-center w-full my-5">
+					<div class="self-center">
+						<h2 class="text-2xl font-bold capitalize mt-5">Værdier</h2>
+					</div>
+					<div class="p-5 self-center">
+						<p>
+						Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet t accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet t accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet t accusam et justo duo dolores et ea.
+						</p>
+					</div>
+					<div class="w-4/5 self-center">
+						<ul>
+							<li class="py-5">
+								<h2 class="font-bold">Value proposition 1.</h2>
+								<p>Lorem ipsum dolor sit amet t accusam et justo duo dolores et ea rebum, ipsum dolor sit amet t accusam et justo duo dolores et ea rebum?</p>
+							</li>
+							<li class="py-5">
+								<h2 class="font-bold">Value proposition 1.</h2>
+								<p>Lorem ipsum dolor sit amet t accusam et justo duo dolores et ea rebum, ipsum dolor sit amet t accusam et justo duo dolores et ea rebum?</p>
+							</li>
+							<li class="py-5">
+								<h2 class="font-bold">Value proposition 1.</h2>
+								<p>Lorem ipsum dolor sit amet t accusam et justo duo dolores et ea rebum, ipsum dolor sit amet t accusam et justo duo dolores et ea rebum?</p>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>			
+		</div>
+		
+	<?php } ?>
+
+	<?php
+			
+			// kontakt content 
+			global $post;
+			if( $post->ID == 137) { ?>
+				<div class="flex flex-col">
+					<div class="self-center bg-white p-5 md:my-5 w-full sm:w-full md:w-2/4">
+						<div class="text-center">
+							<h2 class="text-2xl font-bold text-primary">Kontakt os</h2>
+						</div>
+						<?php the_content(); ?>
+						<div>
+							<ul class="flex justify-between">
+								<li> <p>Tlf: +45 31 51 27 60</p> </li>
+								<li> <p>info@theilmann-it.dk</p> </li>
+								<li> <p>Teglgård 1 6330 Vilsbæk</p> </li>
+							</ul>
+						</div>
+					</div>
+			
+				</div>
+				
+			<?php } ?>
 
 	</div>
 <canvas class="" id="c"></canvas>
